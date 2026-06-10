f209061b8c25397f580cc85c53c25ae512acff0a
kumar ravikar <kumarravikar24@gmail.com>
2026-06-10 13:53:13 +0530

    handling file upload on cloude in imageKit

--- backend/src/controller/food.controller.js
+++ backend/src/controller/food.controller.js
@@ -1,8 +1,10 @@
 import foodModel from "../model/foodItem.model.js"
+import { fileUpload } from "../services/storage.service.js";
 
 export async function createFood(req, res) {
     console.log(req.foodPartner);
     console.log(req.body)
+    const uploadfile = await fileUpload(req.file.buffer)
     console.log(req.file)
     res.send("item created")
     
